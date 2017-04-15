79de30d871d5067859ab23b4a9c3a9ef67a449ac
Carefree4 <carefreefour@gmail.com>
2017-04-15 01:45:04 -0700

    init and ifconfig working

---
+++ NetPack/NetPack/DeviceInformation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcapDotNet.Core;
+
+namespace NetPack
+{
+    public class DeviceInformation
+    {
+        private IList<LivePacketDevice> devices;
+
+        public DeviceInformation()
+        {
+            devices = LivePacketDevice.AllLocalMachine;
+        }
+
+        public override string ToString()
+        {
+            var output = "";
+
+            foreach (var device in devices)
+            {
+                output += $"  Name  . . . . . . : {device.Name}\n";
+                output += $"    Description . . . : {device?.Description}\n";
+                output += "    Loopback  . . . . : " +
+                              (((device.Attributes & DeviceAttributes.Loopback) == DeviceAttributes.Loopback)
+                                   ? "yes"
+                                   : "no") + "\n";
+                foreach (var t in device.Addresses)
+                {
+                    output += $"\n    {t?.Address.Family}\n";
+                    output += $"\t      Address . . . . . : {t?.Address}\n";
+                    output += $"\t      Netmask . . . . . : {t?.Netmask}\n";
+                    output += $"\t      Broadcast . . . . : {t?.Broadcast}\n";
+                    output += $"\t      Destination . . . : {t?.Destination}\n";
+                }
+                output += "\n\n";
+            }
+
+            return output;
+        }
+
+    }
+}
---
+++ NetPack/NetPack/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetPack.Commands;
+
+namespace NetPack
+{
+    class Program
+    {
+        private static Dictionary<string, ICommand> Commands = new Dictionary<string, ICommand>()
+        {
+            {"iflist", new InterfaceListCommand()}
+        };
+
+        static void Main(string[] args)
+        {
+            var active = args.Length <= 0;
+
+            if (active)
+            {
+                Console.Out.Write("Type exit to quit NetPack.");
+            }
+
+            do
+            {
+
+                /*
+                var commandInput = "";
+                if (active)
+                {
+                    Console.Out.Write("\nNetPack: ");
+                    commandInput = Console.ReadLine();
+                } else if (args.Length >= 2)
+                {
+                    commandInput = args[1];
+                }
+
+                if(string.IsNullOrWhiteSpace(commandInput))
+                {
+                    Console.Out.Write(Commands.Keys.ToArray().ToString());
+                }
+
+                if (Commands.TryGetValue(commandInput, out ICommand command))
+                {
+                    command.Execute(args);
+                }
+                else
+                {
+                    Console.Out.WriteLine($"Bad input : {commandInput}");
+                }*/
+                if (active)
+                {
+                    Console.Out.Write("NetPack: ");
+                    var input = Console.ReadLine();
+
+                    if (input == null || input.ToLower().Equals("exit"))
+                    {
+                        active = false;
+                    }
+                    else
+                    {
+                        args = ParseArguments(input);
+                    }
+                }
+
+                if (args.Length <= 0)
+                {
+                    Console.Out.WriteLine("Avalible Commands: ");
+                    foreach (var k in Commands.Keys)
+                    {
+                        Console.Out.WriteLine(k);
+                    }
+                    Console.Out.WriteLine();
+                } else if (Commands.TryGetValue(args[0], out ICommand command))
+                {
+                    command.Execute(args);
+                }
+                else
+                {
+                    Console.Out.WriteLine($"Bad input : {args[0]}");
+                }
+
+            } while (active);
+        }
+
+        static string[] ParseArguments(string commandLine)
+        {
+            if (string.IsNullOrWhiteSpace(commandLine))
+            {
+                return new string[0];
+            }
+
+            var parmChars = commandLine.ToCharArray();
+            var inQuote = false;
+            for (var index = 0; index < parmChars.Length; index++)
+            {
+                if (parmChars[index] == '"')
+                {
+                    inQuote = !inQuote;
+                }
+
+                if (!inQuote && parmChars[index] == ' ')
+                {
+                    parmChars[index] = '\n';
+                }
+            }
+            return (new string(parmChars)).Split('\n');
+        }
+    }
+}
