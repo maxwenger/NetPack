837eb680f9d2fb1ef2afaa3fd3515bcea3c56dc1
Carefree4 <carefreefour@gmail.com>
2017-04-15 13:33:24 -0700

    fixed formating for pings

--- NetPack/NetPack/Services/Ping.cs
+++ NetPack/NetPack/Services/Ping.cs
@@ -35,15 +35,22 @@ namespace NetPack.Services
                 }
             }
 
+            UpdateConsole(replies);
+
+        }
+
+        private void UpdateConsole(Net.PingReply[] replies)
+        {
             if (WriteToConsole)
             {
+                Console.Out.WriteLine($"{"Interface",-20} {"IP",-45} {"TTL",-10} {"Time",-10} {"Status",-10}");
                 for (var i = 0; i < replies.Length; i++)
                 {
                     var r = replies[i];
                     if (r == null)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Out.WriteLine($"{ips[i]} | {Net.IPStatus.TimedOut}");
+                        Console.Out.WriteLine($"{"",-20} {ips[i],-45} {"",-10} {"",-10} {Net.IPStatus.Unknown,-10}");
                     }
                     else
                     {
@@ -51,12 +58,12 @@ namespace NetPack.Services
                             ? ConsoleColor.Green
                             : ConsoleColor.Red;
 
-                        Console.Out.WriteLine($"{r.Address} | {r.Options.Ttl} | {r.RoundtripTime} | {r.Status}");
+                        Console.Out.WriteLine(
+                            $"{"",-20} {ips[i],-45} {r?.Options?.Ttl,-10} {r?.RoundtripTime,-10} {r.Status,-10}");
                     }
                 }
                 Console.ResetColor();
             }
-
         }
     }
 }
